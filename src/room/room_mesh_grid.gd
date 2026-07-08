478a3871c2a2320c2df8a9a74bf8cf24c6f11888
DanilTheFrog <halturin.danil2014@yandex.ru>
2026-07-08 16:23:54 +0400

    toggle grid

--- src/room/room_mesh_grid.gd
+++ src/room/room_mesh_grid.gd
@@ -7,16 +7,26 @@ extends ColorRect
 var tile_size: int = Constants.TEXTURE_RESOLUTION;
 var max_tiles: Vector2 = Vector2(Constants.MAX_TILES_X, Constants.MAX_TILES_Y);
 
+var debug_key: int = KEY_X
+var is_grid_visible: bool = false;
 
 func _ready() -> void:
 	custom_minimum_size = Vector2(tile_size*max_tiles.x, tile_size*max_tiles.y);
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == debug_key:
+			is_grid_visible = !is_grid_visible;
+			queue_redraw()
 
 func _draw() -> void:
-	draw_grid()
-
+	match is_grid_visible:
+		true:
+			draw_grid();
+			
 
 func draw_grid() -> void:
+	
 	# x lines from top to bottom
 	for x in max_tiles.x:
 		draw_line(
